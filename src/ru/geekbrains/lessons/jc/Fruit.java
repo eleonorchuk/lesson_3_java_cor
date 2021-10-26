aea9bef444315749160c418a76685a2f2b6b64da
eleonorchuk <zoimy@ukr.net>
2021-10-27 00:17:15 +0300

    lesson_3_java_cor

---
+++ src/ru/geekbrains/lessons/jc/Fruit.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.lessons.jc;
+
+public abstract class Fruit {
+    public abstract double getWeight();
+
+}
---
+++ src/ru/geekbrains/lessons/jc/Orange.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lessons.jc;
+
+public class Orange extends Fruit{
+    @Override
+    public double getWeight() {
+        return 1.5;
+    }
+}
