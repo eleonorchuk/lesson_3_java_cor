caec018f571531883f1a4a5373dffc6045c5e265
eleonorchuk <zoimy@ukr.net>
2021-10-26 22:49:19 +0300

    lesson_3_java_cor

--- src/ru/geekbrains/lessons/jc/Main.java
+++ src/ru/geekbrains/lessons/jc/Main.java
@@ -1,8 +1,27 @@
 package ru.geekbrains.lessons.jc;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа); */
+        String[] arr = {"abc", "bce", "abd", "cde"};
+        System.out.println("arr = " + Arrays.toString(arr));
+        swapElements(arr,1,3);
+        System.out.println("arr after swap = " + Arrays.toString(arr));
+
+        Integer[] arr1 = {1,2,3,4,5};
+        System.out.println("arr1 = " + Arrays.toString(arr1));
+        // [1,2,3,4,5]
+        swapElements(arr1,1,3);
+        System.out.println("arr1 after swap = " + Arrays.toString(arr1));
+        // [1,4,3,2,5]
+    }
+
+    public static void swapElements(Object[] arr, int index1, int index2) {
+        Object hand = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = hand;
     }
 }
